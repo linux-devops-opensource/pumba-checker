0518f2fa975408db3723ae38b5244cecc4c4581b
root <root@Linuxk8s.xpusxzrzr5nebkq1whbkv2qyph.bx.internal.cloudapp.net>
2021-01-17 18:31:58 +0000

    finished api

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+var axios = require('axios');
+const nexusURL= "http://20.50.53.193:8081"
+const nexusAPI="/service/rest/v1/search?sha1="
+var statusarr =[];
+function fillArray(packageName,statusCode,desc) {
+    var object = {
+        "name": packageName,
+        "statusCode" : statusCode,
+        "status": desc
+      };
+      statusarr.push(object);
+  }
 
-
-const courses = [
-    { id:1,name:'course1'},
-    { id:2,name:'course2'},
-    { id:3,name:'course3'},
-    { id:4,name:'course4'},
-    { id:5,name:'course5'}
-];
-app.get('/', (req,res) => {
-    res.send('Hello World');
-});
-app.get('/api/courses', (req,res) => {
-    res.send(courses)
-});
-app.post('/api/courses', (req,res)=> {
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
+app.post('/api/checkpackages', async (req,res)=> {
+    const data = req.body;
+    const type = data.type;
+    const pkgs = data.packages;
+    for (element of pkgs) {
+        await axios.get(nexusURL+nexusAPI+element.sha1)
+        .then(resp => {
+            if(!resp.data.items.length){
+                fillArray(element.packageName,200,"Package can be uploaded");
+            } else {
+                fillArray(element.packageName,400,"Package is already uploaded");
+            }})
+        .catch(err => {
+            console.log(err);
+        });
+    }
+    res.send(statusarr);
 });
 
-app.get('/api/course/:id', (req,res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send("course not found")
-    res.send(course);
-    
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=> console.log(`listening on port ${port}`));
