0c7db3be829be2fbfd900ccec680e010a2a95d21
root <root@Linuxk8s.xpusxzrzr5nebkq1whbkv2qyph.bx.internal.cloudapp.net>
2021-01-17 21:20:52 +0000

    fixed asyncing and fixed statusarray bug

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ app.use(express.json());
 var axios = require('axios');
 const nexusURL= "http://20.50.53.193:8081"
 const nexusAPI="/service/rest/v1/search?sha1="
-var statusarr =[];
-function fillArray(packageName,statusCode,desc) {
+function fillArray(packageName,statusCode,desc,statusarr) {
     var object = {
         "name": packageName,
         "statusCode" : statusCode,
@@ -13,26 +12,28 @@ function fillArray(packageName,statusCode,desc) {
       };
       statusarr.push(object);
   }
-
 app.post('/api/checkpackages', async (req,res)=> {
     const data = req.body;
     const type = data.type;
     const pkgs = data.packages;
-    for (element of pkgs) {
-        await axios.get(nexusURL+nexusAPI+element.sha1)
-        .then(resp => {
-            if(!resp.data.items.length){
-                fillArray(element.packageName,200,"Package can be uploaded");
-            } else {
-                fillArray(element.packageName,400,"Package is already uploaded");
-            }})
-        .catch(err => {
-            console.log(err);
-        });
+    var statusarr =[];
+    const promises = pkgs.map((element) => {
+    return axios.get(nexusURL + nexusAPI + element.sha1)
+    });
+    const responses = await Promise.all(promises);
+    pkgs.forEach((element, i) => {
+    try {
+      if (!responses[i].data.items.length) {
+        fillArray(element.packageName, 200, "Package can be uploaded",statusarr);
+      } else {
+        fillArray(element.packageName, 400, "Package is already uploaded",statusarr);
+      }
+    } catch (err) {
+      console.log(err);
     }
-    res.send(statusarr);
+    });
+  res.send(statusarr);
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=> console.log(`listening on port ${port}`));
