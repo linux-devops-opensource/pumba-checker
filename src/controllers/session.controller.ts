fd1ca0769a2fc9e0af68c8e72c20d079368e4993
Joshua <shuki.grinman@gmail.com>
2021-07-23 19:39:22 +0300

    added session controller

---
+++ src/controllers/session.controller.ts
@@ -0,0 +1,95 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import {Session} from '../models';
+import {SessionRepository} from '../repositories';
+
+export class SessionsController {
+  constructor(
+    @repository(SessionRepository)
+    public sessionRepository: SessionRepository,
+  ) { }
+
+  @post('/session', {
+    responses: {
+      '200': {
+        description: 'Session model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Session)}},
+      }
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Session, {
+            title: 'NewSession',
+          }),
+        },
+      },
+    })
+    session: Session,
+  ): Promise<Session> {
+      return this.sessionRepository.create(session);
+    }
+
+  @get('/session/{id}', {
+    responses: {
+      '200': {
+        description: 'Session model instance',
+        content: {
+          'application/json': {
+            schema: getModelSchemaRef(Session, {includeRelations: true}),
+          },
+        },
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(Session, {exclude: 'where'}) filter?: FilterExcludingWhere<Session>
+  ): Promise<Session> {
+    return this.sessionRepository.findById(id, filter);
+  }
+
+  @put('/session/{id}', {
+    responses: {
+      '204': {
+        description: 'Session PUT success',
+      },
+    },
+  })
+  async replaceById(
+    @param.path.string('id') id: string,
+    @requestBody() session: Session,
+  ): Promise<void> {
+    await this.sessionRepository.replaceById(id, session);
+  }
+
+  @del('/session/{id}', {
+    responses: {
+      '204': {
+        description: 'Session DELETE success',
+      },
+    },
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.sessionRepository.deleteById(id);
+  }
+}
---
+++ src/models/session.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Session extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    required: true,
+  })
+  sid: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  type: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  statusCode: number;
+
+  @property({
+    type: 'array',
+    itemType: 'object',
+  })
+  pkgs?: object[];
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // [prop: string]: any;
+
+  constructor(data?: Partial<Session>) {
+    super(data);
+  }
+}
+
+export interface SessionRelations {
+  // describe navigational properties here
+}
+
+export type SessionWithRelations = Session & SessionRelations;
